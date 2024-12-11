85daad5d11d9cef23cac2ef9456825f3fbe0b514
Guri <f3u3emgr@yahoo.co.jp>
2024-12-11 18:04:48 +0900

    階層を見やすくレイアウト

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -95,7 +95,9 @@ export function DragDropContext() {
               <div className="flex-auto -mr-px bg-slate-700/50 border border-slate-500/30 rounded-tl"></div>
             </div>
             <div className="absolute top-2 right-0 h-8 flex items-center pr-4"><div className="relative flex -mr-2">
-              <button type="button" className="text-slate-500 hover:text-slate-400"><svg fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true" className="w-8 h-8"><path d="M13 10.75h-1.25a2 2 0 0 0-2 2v8.5a2 2 0 0 0 2 2h8.5a2 2 0 0 0 2-2v-8.5a2 2 0 0 0-2-2H19"></path><path d="M18 12.25h-4a1 1 0 0 1-1-1v-1.5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1.5a1 1 0 0 1-1 1ZM13.75 16.25h4.5M13.75 19.25h4.5"></path></svg></button>
+              <button type="button" className="text-slate-500 hover:text-slate-400">
+                <svg fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true" className="w-8 h-8"><path d="M13 10.75h-1.25a2 2 0 0 0-2 2v8.5a2 2 0 0 0 2 2h8.5a2 2 0 0 0 2-2v-8.5a2 2 0 0 0-2-2H19"></path><path d="M18 12.25h-4a1 1 0 0 1-1-1v-1.5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1.5a1 1 0 0 1-1 1ZM13.75 16.25h4.5M13.75 19.25h4.5"></path></svg>
+              </button>
             </div>
             </div>
           </div>
