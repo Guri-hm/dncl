5643690fff4a707c14bfc02769400d2c467971d1
Guri <f3u3emgr@yahoo.co.jp>
2025-01-06 00:34:12 +0900

    セパレーターの挙動修正中

--- src/app/components/SortableTree.tsx
+++ src/app/components/SortableTree.tsx
@@ -42,6 +42,7 @@ import { processEnum } from "./Dialog/Enum";
 import { Box } from "@mui/material";
 import styles from './alloment-custom.module.css'
 import { ArrowButton } from "./ArrowButton";
+import "./alloment-custom.css";
 
 const initialItems: TreeItems = [
   {
@@ -262,7 +263,7 @@ export function SortableTree({
       onDragCancel={handleDragCancel}
     >
       <DnclEditDialog {...editor}></DnclEditDialog>
-      <Allotment separator={false} defaultSizes={[100, 100]} className={styles.splitViewContainer} onVisibleChange={(_index, value) => {
+      <Allotment separator={true} defaultSizes={[100, 100]} className={styles.splitViewContainer} onVisibleChange={(_index, value) => {
         setVisible(value);
       }}>
         <Allotment.Pane maxSize={50} minSize={50} className={`bg-slate-300`}>
@@ -280,82 +281,90 @@ export function SortableTree({
             ))}
           </Box>
         </Allotment.Pane>
-        <Allotment.Pane className={`${styles.rightPane}`} >
-          <div className="relative z-10 col-span-3 bg-slate-800 rounded-xl shadow-lg xl:ml-0 dark:shadow-none dark:ring-1 dark:ring-inset dark:ring-white/10">
-            <div className="relative flex text-slate-400 text-xs leading-6">
-              <div className="mt-2 flex-none text-sky-300 border-t border-b border-t-transparent border-b-sky-300 px-4 py-1 flex items-center">DNCL</div>
-              <div className="flex-auto flex pt-2 rounded-tr-xl overflow-hidden">
-                <div className="flex-auto -mr-px bg-slate-700/50 border border-slate-500/30 rounded-tl"></div>
+
+        <div style={{ margin: '20px' }}>
+
+          <Allotment.Pane className={`${styles.rightPane}`} >
+            <div className="h-full border border-gray-600 relative z-10 col-span-3 bg-slate-800 rounded-xl shadow-lg xl:ml-0 dark:shadow-none dark:ring-1 dark:ring-inset dark:ring-white/10">
+              <div className="relative flex text-slate-400 text-xs leading-6">
+                <div className="mt-2 flex-none text-sky-300 border-t border-b border-t-transparent border-b-sky-300 px-4 py-1 flex items-center">DNCL</div>
+                <div className="flex-auto flex pt-2 rounded-tr-xl overflow-hidden">
+                  <div className="flex-auto -mr-px bg-slate-700/50 border border-slate-500/30 rounded-tl"></div>
+                </div>
+                <div className="absolute top-2 right-0 h-8 flex items-center pr-4"><div className="relative flex -mr-2">
+                  <button type="button" className="text-slate-500 hover:text-slate-400">
+                    <svg fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true" className="w-8 h-8"><path d="M13 10.75h-1.25a2 2 0 0 0-2 2v8.5a2 2 0 0 0 2 2h8.5a2 2 0 0 0 2-2v-8.5a2 2 0 0 0-2-2H19"></path><path d="M18 12.25h-4a1 1 0 0 1-1-1v-1.5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1.5a1 1 0 0 1-1 1ZM13.75 16.25h4.5M13.75 19.25h4.5"></path></svg>
+                  </button>
+                </div>
+                </div>
               </div>
-              <div className="absolute top-2 right-0 h-8 flex items-center pr-4"><div className="relative flex -mr-2">
-                <button type="button" className="text-slate-500 hover:text-slate-400">
-                  <svg fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true" className="w-8 h-8"><path d="M13 10.75h-1.25a2 2 0 0 0-2 2v8.5a2 2 0 0 0 2 2h8.5a2 2 0 0 0 2-2v-8.5a2 2 0 0 0-2-2H19"></path><path d="M18 12.25h-4a1 1 0 0 1-1-1v-1.5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1.5a1 1 0 0 1-1 1ZM13.75 16.25h4.5M13.75 19.25h4.5"></path></svg>
-                </button>
-              </div>
+              <div className="relative text-white">
+                <SortableContext items={sortedIds} strategy={verticalListSortingStrategy}>
+                  {flattenedItems.map(({ id, children, collapsed, depth, code }) => (
+                    <SortableTreeItem
+                      key={id}
+                      id={id}
+                      value={code}
+                      depth={id === activeId && projected ? projected.depth : depth}
+                      indentationWidth={indentationWidth}
+                      indicator={indicator}
+                      collapsed={Boolean(collapsed && children.length)}
+                      onCollapse={
+                        collapsible && children.length
+                          ? () => handleCollapse(id)
+                          : undefined
+                      }
+                      onRemove={removable ? () => handleRemove(id) : undefined}
+                    />
+                  ))}
+                </SortableContext>
               </div>
             </div>
-            <div className="relative text-white">
-              <SortableContext items={sortedIds} strategy={verticalListSortingStrategy}>
-                {flattenedItems.map(({ id, children, collapsed, depth, code }) => (
+            {createPortal(
+              <DragOverlay
+                dropAnimation={dropAnimation}
+                modifiers={indicator ? [adjustTranslate] : undefined}
+              >
+                {activeId && activeItem ? (
                   <SortableTreeItem
-                    key={id}
-                    id={id}
-                    value={code}
-                    depth={id === activeId && projected ? projected.depth : depth}
+                    id={activeId}
+                    depth={activeItem.depth}
+                    clone
+                    childCount={getChildCount(items, activeId) + 1}
+                    value={activeCode ? activeCode : ""}
                     indentationWidth={indentationWidth}
-                    indicator={indicator}
-                    collapsed={Boolean(collapsed && children.length)}
-                    onCollapse={
-                      collapsible && children.length
-                        ? () => handleCollapse(id)
-                        : undefined
-                    }
-                    onRemove={removable ? () => handleRemove(id) : undefined}
                   />
-                ))}
-              </SortableContext>
-            </div>
-          </div>
-          {createPortal(
-            <DragOverlay
-              dropAnimation={dropAnimation}
-              modifiers={indicator ? [adjustTranslate] : undefined}
-            >
-              {activeId && activeItem ? (
-                <SortableTreeItem
-                  id={activeId}
-                  depth={activeItem.depth}
-                  clone
-                  childCount={getChildCount(items, activeId) + 1}
-                  value={activeCode ? activeCode : ""}
-                  indentationWidth={indentationWidth}
-                />
-              ) : null}
-            </DragOverlay>,
-            document.body
-          )}
-        </Allotment.Pane>
+                ) : null}
+              </DragOverlay>,
+              document.body
+            )}
+          </Allotment.Pane>
 
+        </div>
 
-        <Allotment.Pane>
-          <div className="relative z-10 col-span-3 bg-slate-800 rounded-xl shadow-lg xl:ml-0 dark:shadow-none dark:ring-1 dark:ring-inset dark:ring-white/10">
-            <div className="relative flex text-slate-400 text-xs leading-6">
-              <div className="mt-2 flex-none text-sky-300 border-t border-b border-t-transparent border-b-sky-300 px-4 py-1 flex items-center">Python</div>
-              <div className="flex-auto flex pt-2 rounded-tr-xl overflow-hidden">
-                <div className="flex-auto -mr-px bg-slate-700/50 border border-slate-500/30 rounded-tl"></div>
+        <div style={{ margin: '20px' }}>
+
+          <Allotment.Pane>
+            <div className="h-full border border-gray-600 relative z-10 col-span-3 bg-slate-800 rounded-xl shadow-lg xl:ml-0 dark:shadow-none dark:ring-1 dark:ring-inset dark:ring-white/10">
+              <div className="relative flex text-slate-400 text-xs leading-6">
+                <div className="mt-2 flex-none text-sky-300 border-t border-b border-t-transparent border-b-sky-300 px-4 py-1 flex items-center">Python</div>
+                <div className="flex-auto flex pt-2 rounded-tr-xl overflow-hidden">
+                  <div className="flex-auto -mr-px bg-slate-700/50 border border-slate-500/30 rounded-tl"></div>
+                </div>
+                <div className="absolute top-2 right-0 h-8 flex items-center pr-4"><div className="relative flex -mr-2">
+                  <button type="button" className="text-slate-500 hover:text-slate-400">
+                    <svg fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true" className="w-8 h-8"><path d="M13 10.75h-1.25a2 2 0 0 0-2 2v8.5a2 2 0 0 0 2 2h8.5a2 2 0 0 0 2-2v-8.5a2 2 0 0 0-2-2H19"></path><path d="M18 12.25h-4a1 1 0 0 1-1-1v-1.5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1.5a1 1 0 0 1-1 1ZM13.75 16.25h4.5M13.75 19.25h4.5"></path></svg>
+                  </button>
+                </div>
+                </div>
               </div>
-              <div className="absolute top-2 right-0 h-8 flex items-center pr-4"><div className="relative flex -mr-2">
-                <button type="button" className="text-slate-500 hover:text-slate-400">
-                  <svg fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true" className="w-8 h-8"><path d="M13 10.75h-1.25a2 2 0 0 0-2 2v8.5a2 2 0 0 0 2 2h8.5a2 2 0 0 0 2-2v-8.5a2 2 0 0 0-2-2H19"></path><path d="M18 12.25h-4a1 1 0 0 1-1-1v-1.5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1.5a1 1 0 0 1-1 1ZM13.75 16.25h4.5M13.75 19.25h4.5"></path></svg>
-                </button>
-              </div>
+              <div className="relative text-white">
+
               </div>
             </div>
-            <div className="relative text-white">
+          </Allotment.Pane>
+        </div>
 
-            </div>
-          </div>
-        </Allotment.Pane>
       </Allotment>
     </DndContext>
   );
--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -2,11 +2,15 @@
 import * as React from "react";
 import { Allotment } from "allotment";
 import "allotment/dist/style.css";
+import "../components/alloment-custom.css";
 import { SortableTree } from "../components/SortableTree";
 export default function Home() {
 
   return (
-    <div className="w-full border-solid border-2 h-dvh">
+    <div className="w-full border-solid border-2 h-dvh bg-slate-800 rounded-xl overflow-hidden">
+      <div className="relative flex text-slate-400 text-xs leading-6">
+        <div className="mt-2 flex-none text-sky-300 px-4 py-1 flex items-center">どんな ニーズも カバーしちゃう ロジック！ ⇒ DNCL</div>
+      </div>
       <Allotment vertical defaultSizes={[200, 100]}>
         <Allotment.Pane>
           <SortableTree collapsible indicator removable ></SortableTree>
