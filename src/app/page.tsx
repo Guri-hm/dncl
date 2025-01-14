910007c4a0c4d38e2639a31b2ffd63b852ed7a93
Guri <f3u3emgr@yahoo.co.jp>
2025-01-14 17:41:00 +0900

    テスト中

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,27 @@
 import React, { useEffect, useState } from 'react';
 import Kuroshiro from 'kuroshiro';
 import KuromojiAnalyzer from '@sglkc/kuroshiro-analyzer-kuromoji';
+import { ESLint } from 'eslint';
 
 export default function Home() {
 
   const [convertedText, setConvertedText] = useState('');
   let text = "桜さく";
+  const [code, setCode] = useState('');
+  const [results, setLintResults] = useState<string>('');
+
+  const fetchLintResults = async () => {
+
+    try {
+      const res = await fetch('/api/lint');
+      const data = await res.json();
+      console.log(data);
+      setLintResults(data.resultText);
+    } catch (error) {
+      console.error('Error fetching lint results:', error);
+    }
+
+  };
 
   useEffect(() => {
     const convertText = async () => {
@@ -22,10 +38,25 @@ export default function Home() {
   }, [text]);
 
   return (
-    <div>
-      <p>Original: {text}</p>
-      <p>Converted: {convertedText}</p>
-    </div>
+    <>
+      <textarea
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+        rows={10}
+        cols={50}
+        placeholder="ここにコードを入力してください"
+      />
+      <button onClick={fetchLintResults}>評価する</button>
+      <div>
+        <p>Original: {text}</p>
+        <p>Converted: {convertedText}</p>
+      </div>
+      <div>
+        {
+          results
+        }
+      </div>
+    </>
   );
 }
 
