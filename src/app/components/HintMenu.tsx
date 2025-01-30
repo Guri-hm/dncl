94f360e982e372972204e66871b1d915461abae4
Guri <f3u3emgr@yahoo.co.jp>
2025-01-30 22:56:29 +0900

    代入文

--- src/app/components/HintMenu.tsx
+++ src/app/components/HintMenu.tsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 import { List, ListItem, ListItemText, Box, Button } from '@mui/material';
+import { NextImage } from './NextImage';
+import Image from 'next/image';
 
 const menuItems = [
-    { id: 1, text: '代入文', description: <div>アイテム 1 の説明</div> },
+    {
+        id: 1, text: '代入文', description:
+            <div style={{ width: '300%', height: '300%' }}><Image src={"/assignment.svg"} width={300} height={300} alt='代入' objectFit='contain'></Image></div>
+    },
     { id: 2, text: 'アイテム 2', description: <div>アイテム 2 の説明</div> },
     { id: 3, text: 'アイテム 3', description: <div>アイテム 3 の説明</div> },
 ];
@@ -21,13 +26,16 @@ const HintMenu = () => {
     return (
         <Box>
             {selectedItem === null ? (
-                <List>
-                    {menuItems.map((item) => (
-                        <ListItem key={item.id} onClick={() => handleClick(item.id)}>
-                            <ListItemText primary={item.text} />
-                        </ListItem>
-                    ))}
-                </List>
+                <>
+                    <List>
+                        {menuItems.map((item) => (
+                            <ListItem key={item.id} onClick={() => handleClick(item.id)}>
+                                <ListItemText primary={item.text} />
+                            </ListItem>
+                        ))}
+                    </List>
+
+                </>
             ) : (
                 <Box>
                     {menuItems.find(item => item.id === selectedItem)?.description}
