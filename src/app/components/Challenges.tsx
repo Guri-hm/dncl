f34e8c5f7db60b3145e6154fd24a1dbd502c208a
Guri <f3u3emgr@yahoo.co.jp>
2025-02-08 12:08:42 +0900

    表示文の問題を作成

--- src/app/components/Challenges.tsx
+++ src/app/components/Challenges.tsx
@@ -21,20 +21,11 @@ const practiceAssignment: Challenge = {
 };
 const practiceOutput: Challenge = {
     id: "2",
-    items: [
-        {
-            id: uuidv4(),
-            line: "a ← 1",
-            children: [],
-            lineTokens: ["a"],
-            processIndex: ProcessEnum.Output,
-            fixed: true
-        }
-    ],
-    title: '表示文',
-    task: "コンソールに'30'と表示しましょう",
-    hint: "代入文を使い，変数aに値を入れます",
-    answer: ["30"],
+    items: [],
+    title: '表示文の使い方',
+    task: "コンソールに'こんにちは'と表示しましょう",
+    hint: "表示文で変数または文字列を表示します",
+    answer: ["こんにちは"],
 };
 
 export const allChallengesItems: Challenge[] = [
