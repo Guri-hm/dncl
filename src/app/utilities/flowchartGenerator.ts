ce2acf4d4aee9067439def18f276ac56024d1b8e
Guri <f3u3emgr@yahoo.co.jp>
2025-02-13 22:57:17 +0900

    位置を整理

--- src/app/utilities/flowchartGenerator.ts
+++ src/app/utilities/flowchartGenerator.ts
@@ -25,17 +25,20 @@ export const generateFlowchartXML = (ast: ASTNode) => {
 
     let nodeId = 2;
     let edgeId = 1000; // エッジ用のIDを別に管理
-
+    let maxY = 0; //ノード追加時にy座標を記録
+    const drawX = 240;
+    const nodeDefaultHeight = 30;
     //ノードの真下
     let exitXY: { x: number, y: number } | null = null;
     let lastNodeId: number;
 
-    const createNode = (value: string, style: string, x: number, y: number, width: number = 120, height: number = 60): string => {
+    const createNode = (value: string, style: string, x: number, y: number, width: number = 120, height: number = nodeDefaultHeight): string => {
         const node = `
     <mxCell id="${nodeId}" value="${escape(value)}" style="${style}" vertex="1" parent="1">
       <mxGeometry x="${x}" y="${y}" width="${width}" height="${height}" as="geometry" />
     </mxCell>
     `;
+        maxY = y;
         nodeId++;
         lastNodeId = nodeId;
         return node;
@@ -58,7 +61,7 @@ export const generateFlowchartXML = (ast: ASTNode) => {
         return edge;
     };
 
-    const addNode = (value: string, style: string, x: number, y: number, previousNodeId: number | null, width: number = 120, height: number = 60) => {
+    const addNode = (value: string, style: string, x: number, y: number, previousNodeId: number | null, width: number = 120, height: number = nodeDefaultHeight) => {
         const node = createNode(value, style, x, y, width, height);
         xml += node;
         if (previousNodeId !== null) {
@@ -108,7 +111,8 @@ export const generateFlowchartXML = (ast: ASTNode) => {
                 // 真の分岐
                 if (node.consequent && node.consequent.body) {
                     node.consequent.body.forEach((consequentNode: ASTNode, index: number) => {
-                        processNode(consequentNode, x, y + 120 * (index + 1), index == 0 ? ifNodeId : nodeId - 1);
+                        console.log(`条件分岐y:${y + 30 * (index + 1)}`)
+                        processNode(consequentNode, x, y + 30 * (index + 1), index == 0 ? ifNodeId : nodeId - 1);
                         lastNodeId = nodeId - 1;
                     });
                     nodeIds.push(lastNodeId);
@@ -134,8 +138,8 @@ export const generateFlowchartXML = (ast: ASTNode) => {
                 if (!node.alternate) {
                     const wayPoint = `
                             <Array as="points">
-                            <mxPoint x="400" y="290" />
-                            <mxPoint x="400" y="500" />
+                            <mxPoint x="${x + 200}" y="${y + 15}" />
+                            <mxPoint x="${x + 200}" y="${maxY}" />
                             </Array>
                     `
                     xml += createEdge(ifNodeId, mergeNodeId, 'endArrow=none;', wayPoint);
@@ -148,24 +152,30 @@ export const generateFlowchartXML = (ast: ASTNode) => {
                 const whileTestString = `${whileTest.left.name} ${whileTest.operator} ${whileTest.right.value}`;
 
                 // ループ開始端子
-                addNode(`${whileTestString}の間`, 'strokeWidth=1;html=1;shape=mxgraph.flowchart.loop_limit;whiteSpace=wrap;', x, y + 60, nodeId - 1);
+                console.log(`ループ開始y:${y + 30}`)
+                addNode(`${whileTestString}の間`, 'strokeWidth=1;html=1;shape=mxgraph.flowchart.loop_limit;whiteSpace=wrap;', x, y + 30, nodeId - 1);
 
                 let bodyLength: number = 0;
+
                 if (node.body) {
                     if (Array.isArray(node.body)) {
                         node.body.forEach((bodyNode: ASTNode, index: number) => {
-                            processNode(bodyNode, x, y + 120 * (index + 1), null);
+                            console.log(`ループ内要素${(index + 1)}の開始y:${y + 90 * (index + 1)}`)
+                            processNode(bodyNode, x, y + 90 * (index + 1), null);
                         });
                         bodyLength = node.body.length;
                     } else if (Array.isArray(node.body.body)) {
                         node.body.body.forEach((bodyNode: ASTNode, index: number) => {
-                            processNode(bodyNode, x, y + 120 * (index + 1), null);
+                            console.log(`ループ内要素${(index + 1)}の開始y:${y + 90 * (index + 1)}`)
+                            processNode(bodyNode, x, y + 90 * (index + 1), null);
                         });
                         bodyLength = node.body.body.length;
                     }
                 };
+
                 // ループ終了端子
-                addNode('', 'strokeWidth=1;html=1;shape=mxgraph.flowchart.loop_limit;whiteSpace=wrap;flipH=0;flipV=1;', x, y + 120 * (bodyLength + 1), nodeId - 1);
+                console.log(`ループ開始y:${y + 60 + 90 * (bodyLength)}`)
+                addNode('', 'strokeWidth=1;html=1;shape=mxgraph.flowchart.loop_limit;whiteSpace=wrap;flipH=0;flipV=1;', x, y + 60 + 90 * (bodyLength), nodeId - 1);
 
                 break;
             default:
@@ -189,15 +199,15 @@ export const generateFlowchartXML = (ast: ASTNode) => {
         }
     };
 
-    addNode('開始', 'html=1;dashed=0;whiteSpace=wrap;shape=mxgraph.dfd.start', 240, 30, null);
+    addNode('開始', 'html=1;dashed=0;whiteSpace=wrap;shape=mxgraph.dfd.start', drawX, 30, null);
 
     if (Array.isArray(ast.body)) {
         ast.body.forEach((node: ASTNode, index: number) => {
-            processNode(node, 240, 130 * (index + 1), null);
+            processNode(node, drawX, 90 * (index + 1), null);
         });
     }
 
-    addNode('終了', 'html=1;dashed=0;whiteSpace=wrap;shape=mxgraph.dfd.start', 240, 550, nodeId - 1);
+    addNode('終了', 'html=1;dashed=0;whiteSpace=wrap;shape=mxgraph.dfd.start', drawX, maxY + 30, nodeId - 1);
     xml += `
   </root>
 </mxGraphModel>
