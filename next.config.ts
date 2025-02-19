7052ab9618a9623b9639c62ea09de728d5e952c5
Guri <f3u3emgr@yahoo.co.jp>
2025-02-20 08:20:20 +0900

    SCSSのコンパイルが非推奨だったので更新

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    api: 'modern-compiler' // モダンAPIを使用するように設定
+  },
+  /* 他の設定オプションはこちらに追加 */
 };
 
 export default nextConfig;
