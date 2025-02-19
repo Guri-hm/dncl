8d27a569bb6d61cd2ab86e878c204d49bfdf120a
Guri <f3u3emgr@yahoo.co.jp>
2025-02-19 18:26:29 +0900

    画像表示とレスポンシブ対応

--- src/app/components/DropHere.tsx
+++ src/app/components/DropHere.tsx
@@ -1,18 +1,39 @@
 import React from 'react';
 import Spotlight from './Spotlight';
 import SpeechBubbleImage from './SpeechBubbleImage';
-import { Box } from '@mui/material';
+import { Box, createTheme, useMediaQuery } from '@mui/material';
 
 interface Props {
     visible?: boolean;
 }
+
+interface ResponsiveImageProps {
+    src: string;
+    alt: string;
+}
+const theme = createTheme();
+
+const ResponsiveImage: React.FC<ResponsiveImageProps> = ({ src, alt }) => {
+    const isSm = useMediaQuery(theme.breakpoints.up('sm'));
+
+    let width;
+    if (isSm) {
+        width = '100%';
+    } else {
+        width = '70%';
+    }
+
+    return (
+        <img src={src} alt={alt} style={{ width }} />
+    );
+};
 
 const DropHere = ({ visible = true }: Props) => {
     return (
         <Box sx={{ display: `${visible ? 'block' : 'none'}` }}>
             <Spotlight>
                 <SpeechBubbleImage msg='ここにドロップします'>
-                    <img src="/raise_one_hand.svg" alt="手をあげる" style={{ width: '100%' }}></img>
+                    <ResponsiveImage src="/raise_one_hand.svg" alt="手をあげる"></ResponsiveImage>
                 </SpeechBubbleImage>
             </Spotlight>
         </Box>
--- src/app/components/Spotlight.tsx
+++ src/app/components/Spotlight.tsx
@@ -1,21 +1,30 @@
-// components/ScopeBox.tsx
 import React from 'react';
 import Box from '@mui/material/Box';
-import { BoxProps } from '@mui/system';
+import { BoxProps, createTheme, useMediaQuery } from '@mui/system';
 
+const theme = createTheme();
 const Spotlight: React.FC<BoxProps> = ({ children }) => {
-    return <Box
-        sx={{
-            width: '80%', maxWidth: '600px', height: '90%', maxHeight: '600px', borderRadius: '50%', marginX: 'auto',
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            boxShadow: '0 0 20px rgba(0, 0, 0, 0.5)', // 周囲の影
-            background: 'radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 70%)', // スポットライトの効果
-        }}
-    >{children}
-    </Box>;
+    const isSm = useMediaQuery(theme.breakpoints.down('sm'));
+    return (
+        <Box
+            sx={{
+                width: isSm ? '60%' : '80%',
+                maxWidth: isSm ? '300px' : '600px',
+                height: isSm ? '60%' : '90%',
+                maxHeight: isSm ? '300px' : '600px',
+                borderRadius: '50%',
+                marginX: 'auto',
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                boxShadow: '0 0 20px rgba(0, 0, 0, 0.5)', // 周囲の影
+                background: 'radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 70%)', // スポットライトの効果
+            }}
+        >
+            {children}
+        </Box>
+    );
 };
 
 export default Spotlight;
