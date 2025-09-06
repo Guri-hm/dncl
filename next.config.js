a39b5ef603f8596863f0abed9ab2d107a9ced7ea
Guri <f3u3emgr@yahoo.co.jp>
2025-09-07 05:57:47 +0900

    JSファイルでTypeScript型注釈を使用

--- next.config.js
+++ next.config.js
@@ -1,10 +1,62 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  swcMinify: true,
   trailingSlash: true,
+
+  // 環境別basePath設定
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH && process.env.NEXT_PUBLIC_BASE_PATH !== ''
+    ? process.env.NEXT_PUBLIC_BASE_PATH
+    : undefined,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH && process.env.NEXT_PUBLIC_BASE_PATH !== ''
+    ? process.env.NEXT_PUBLIC_BASE_PATH
+    : undefined,
+
   images: {
     unoptimized: true
-  }
+  },
+
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    api: 'modern-compiler'
+  },
+
+  experimental: {
+    optimizePackageImports: ['@mui/material', '@mui/icons-material'],
+  },
+
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.splitChunks = {
+        ...config.optimization.splitChunks,
+        cacheGroups: {
+          default: false,
+          vendors: false,
+          mui: {
+            name: 'mui',
+            test: /[\\/]node_modules[\\/]@mui[\\/]/,
+            chunks: 'all',
+            priority: 10,
+          },
+          heavy: {
+            name: 'heavy-components',
+            test: /[\\/]src[\\/]app[\\/]components[\\/](Tab|SortableTree|Challenge)[\\/]/,
+            chunks: 'all',
+            priority: 5,
+          },
+          common: {
+            name: 'common',
+            minChunks: 2,
+            chunks: 'all',
+            priority: 1,
+          },
+        },
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
