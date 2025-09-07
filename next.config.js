b07b7474272cef9f278931986ad13f46d2b23ce8
Guri <f3u3emgr@yahoo.co.jp>
2025-09-07 12:31:22 +0900

    設定周り修正

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  swcMinify: true,
 
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
